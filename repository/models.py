3ab09c50999603f80b80c7949da7f39ced4e15b7
Gabriel Müller <gabrielgoetzmuller@gmail.com>
2021-09-02 01:51:27 -0300

    FEAT: Starred repos are now saved in database.
    Pertinent info of the repos are checked and update for each interaction

--- repository/models.py
+++ repository/models.py
@@ -2,23 +2,26 @@ from django.db import models
 from django.db.models.deletion import CASCADE
 from django.db.models.fields import (
     CharField,
-    IntegerField
+    IntegerField,
+    TextField
 )
 from django.db.models.fields.related import ForeignKey
 from django.contrib.auth.models import User
 
 
 class RepositoryManager(models.Manager):
-    def create_repository(self, name, user_id, repo_id):
-        repository = self.create(name=name, user_id=user_id, repo_id=repo_id)
+    def create_repository(self, name, user, repo_id, description):
+        repository = self.create(name=name, user=user,
+        repo_id=repo_id, description=description)
         # do something if needed
         return repository
 
 
 class Repository(models.Model):
     name = CharField(max_length=255, verbose_name='Name')
-    user_id = ForeignKey(User, on_delete=CASCADE)
+    user = ForeignKey(User, on_delete=CASCADE)
     repo_id = IntegerField()
+    description = TextField(null=True)
 
     objects = RepositoryManager()
 
@@ -26,9 +29,8 @@ class Repository(models.Model):
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['user_id', 'repo_id'], name='user_repo_id_constraint')
+            models.UniqueConstraint(fields=['user', 'repo_id'], name='user_repo_id_constraint')
         ]
 
     def __str__(self):
         return self.name
-
--- repository/views.py
+++ repository/views.py
@@ -1,10 +1,51 @@
+import requests
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from social_django.models import UserSocialAuth
+
+from . models import Repository
+
+def request_starred_repos(request, user):
+    try:
+        starred_repos = requests.get(
+        'https://api.github.com/users/{user}/starred'.format(user=user)
+        )
+        starred_repos.raise_for_status()
+    except requests.exceptions.RequestException as err:
+        messages.error(request, err)
+        return
+    except requests.exceptions.HTTPError as err:
+        messages.error(request, err)
+        return
+
+    # For each starred repo
+    for s_repo in starred_repos.json():
+        repo = Repository.objects.get(repo_id=s_repo['id'], user_id=user)
+        # Check if not exists before creating new record
+        if not repo:
+            repo = Repository.objects.create_repository(
+                s_repo['name'],
+                user,
+                s_repo['id'],
+                s_repo['description']
+            )
+            repo.save()
+        # Check if repo name was changed
+        elif repo.name != s_repo['name']:
+            repo.name = s_repo['name']
+            repo.save()
+        # Check if repo description was changed
+        elif repo.description != s_repo['description']:
+            repo.description = s_repo['description']
+            repo.save()
+
 
 @login_required(redirect_field_name='login')
 def home(request):
     user = request.user
+
+    request_starred_repos(request, user)
 
     try:
         github_login = user.social_auth.get(provider='github')
